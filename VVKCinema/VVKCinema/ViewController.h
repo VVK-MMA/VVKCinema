6d125627e2f1c29ea2c2289689ba184a51c477c1
valmanchev <vmanchev@yahoo.com>
2015-05-24 15:28:13 +0300

    added new Single View Application

---
+++ VVKCinema/VVKCinema/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VVKCinema
+//
+//  Created by Valeri Manchev on 5/24/15.
+//  Copyright (c) 2015 VVK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

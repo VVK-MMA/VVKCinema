29c247279e583a5f6c8f68f970ba0fbad5d083c9
KaloyanPetrov <engineer_kaloyan_petrov@abv.bg>
2015-05-27 13:36:22 -0700

    New Commit

--- VVKCinema/VVKCinema/AppDelegate.m
+++ VVKCinema/VVKCinema/AppDelegate.m
@@ -16,6 +16,8 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    NSLog(@"Vlado iska da skube na Trayana pod mishnicite");
     // Override point for customization after application launch.
     return YES;
 }

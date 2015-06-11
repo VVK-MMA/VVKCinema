360f1f39352efd6bd1865d3c9480d002d8f0a5bd
valmanchev <vmanchev@yahoo.com>
2015-06-11 18:14:34 +0300

    added Parse Class and fetching methods that fetch data from server

---
+++ VVKCinema/VVKCinema/Controllers/Parse.h
@@ -0,0 +1,23 @@
+//
+//  Parse.h
+//  VVKCinema
+//
+//  Created by Valeri Manchev on 6/11/15.
+//  Copyright (c) 2015 VVK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Parse : NSObject
+
+@property (strong, nonatomic) NSDictionary *responseDictionary;
+
++ (id)sharedParse;
+
+- (NSDictionary *)getAllObjectsWithType:(NSString *)type relatedToObjectWithClassName:(NSString *)className objectId:(NSString *)objectId andKeyName:(NSString *)keyName;
+
+- (void)getAllObjectsWithType:(NSString *)type;
+
+- (NSDictionary *)getObjectWithType:(NSString *)type andObjectId:(NSString *)objectId;
+
+@end

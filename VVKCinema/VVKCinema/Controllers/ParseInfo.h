0a42067a2a0137bd7bfb9c7afb00eeb4a2b04065
valmanchev <vmanchev@yahoo.com>
2015-06-23 19:59:52 +0300

    added all comments for ParseInfo class

--- VVKCinema/VVKCinema/Controllers/ParseInfo.h
+++ VVKCinema/VVKCinema/Controllers/ParseInfo.h
@@ -41,14 +41,48 @@
  *  Send sign up request (register)
  *
  *  @param name     name of the new User
- *  @param password password of the new User ()
- *  @param email    <#email description#>
+ *  @param password password of the new User (no access to the password on the server)
+ *  @param email    email of the new User (unique)
  */
 - (void)sendSignUpRequestToParseWithName:(NSString *)name password:(NSString *)password andEmail:(NSString *)email;
+
+/**
+ *  Create new Seat object to the server
+ *
+ *  @param column       column of the new Seat
+ *  @param row          row of the new Seat
+ *  @param projectionId projectionId of the Projection where the new Seat is
+ */
 - (void)bookNewSeatToParseWithColumn:(NSNumber *)column row:(NSNumber *)row andProjectionId:(NSString *)projectionId;
+
+/**
+ *  Create new Ticket object to the server
+ *
+ *  @param seat       seat of the new Ticket
+ *  @param ticketType ticketType of the new Ticket (regular or student)
+ *  @param userId     userId of the logged in User who book the new Ticket
+ */
 - (void)bookNewTicketToParseWithSeat:(NSString *)seat ticketType:(NSString *)ticketType andUserId:(NSString *)userId;
 
+/**
+ *  Send post request to the server to login
+ *
+ *  @param username username of the existing User (same like the email - unique)
+ *  @param password password of the existing User (no access to the password on the server - hidden)
+ *
+ *  @return dictionary with information about the User from the server
+ */
 - (NSDictionary *)loginUserWithUsername:(NSString *)username andPassword:(NSString *)password;
+
+/**
+ *  Take the booked Seat from the current Projection
+ *
+ *  @param column     column of the booked Seat
+ *  @param row        row of the booked Seat
+ *  @param projection id of the Projection where the booked Seat is
+ *
+ *  @return dictionary with information about the booked Seat from the server
+ */
 - (NSDictionary *)getSeatWithClassName:(NSString *)className column:(NSNumber *)column row:(NSNumber *)row fromProjection:(NSString *)projection;
 
 @end
